25627b13486fbc6ee4445459162b6f532c38954b
I3enTi <timothy.f.chen@gmail.com>
2014-04-18 14:41:06 -0700

    Update 1.1
    Now, if user does not have require folder path, it is created for them. All they have to do is import and rename background image.

--- LogonBckgrnd.bat
+++ LogonBckgrnd.bat
@@ -1,12 +1,29 @@
+@echo off 
+
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background /v OEMBackground /t REG_DWORD /d 1 /f
 
-start C:\Windows\System32\oobe\info\backgrounds
+set b_folder=%WINDIR%\System32\oobe\info\backgrounds
 
+IF EXIST %b_folder% (
+
+start %b_folder%
+@echo on  ECHO: exists 
+@echo off
+
+) ELSE (
+
+ md %b_folder% 
+ start %b_folder% 
+@echo on ECHO: does NOT exist; made 
+ )
+ 
+ECHO: COPY YOUR (JPEG)BACKGROUND IMAGE HERE and NAME IT "backgroundDefault"
 pause
 
 @echo off
+
 :implementChange
-set /p Change=Implement Background Change [y/n] ?:
+set /p Change=Implement Background Change [y/n]?:
 if %Change%==y logoff
 if %Change%==n goto :close
 
